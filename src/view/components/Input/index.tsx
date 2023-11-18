472d2db7106088550239566e3196813ae0ae7e85
Vinicius Braznick <viniciusbraznick@gmail.com>
2023-11-18 13:28:50 -0300

    feat: adding function to toggle password visibility

--- src/view/components/Input/index.tsx
+++ src/view/components/Input/index.tsx
@@ -1,17 +1,25 @@
-import { ComponentProps } from "react";
-import { Fieldset, Label } from "./styles";
+import { ComponentProps, useState } from "react";
+import { ButtonIcon, Fieldset, Label } from "./styles";
+import { EyeOpenIcon, EyeClosedIcon } from '@radix-ui/react-icons';
 
 interface InputProps extends ComponentProps<'input'> {}
 
-export function Input({placeholder, name, id, ...props}: InputProps) {
+export function Input({placeholder, name, id, type, ...props}: InputProps) {
+  const [inputType, setInputType] = useState(type);
   const inputID = id ?? name;
+
   return(
     <Fieldset>
-      <input {...props} name={name} placeholder=" " id={inputID} />
+      <input {...props} name={name} type={inputType} placeholder=" " id={inputID} />
       <Label htmlFor={inputID}>
         {placeholder}
       </Label>
 
+      {inputID === 'password' && (
+        <ButtonIcon type="button" onClick={() => setInputType(inputType === 'password' ? 'text' : 'password')}>
+          {inputType === 'password' ? <EyeClosedIcon width={20} height={20} /> : <EyeOpenIcon width={20} height={20}/>}
+        </ButtonIcon>
+      )}
     </Fieldset>
   )
 }
