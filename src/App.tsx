a1a223dfd5a36071d244ed2ae4e60174ac983db5
Vinicius Braznick <viniciusbraznick@gmail.com>
2023-11-28 20:57:47 -0300

    feat: creating auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import { Router } from "./Router"
 import { globalStyles } from "./view/styles/globalStyles";
+import { Toaster } from 'react-hot-toast';
+import { AuthProvider } from './app/contexts/AuthContext';
+
+
+const queryClient = new QueryClient();
 
 function App() {
   globalStyles();
   return (
-    <>
-    <Router />
-    </>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router />
+      </AuthProvider>
+      <Toaster toastOptions={{
+        style: {
+          background: '#363636',
+          color: '#fff',
+          fontFamily: 'var(--fonts-outfit)'
+        }
+      }}/>
+    </QueryClientProvider>
   )
 }
 
---
+++ src/app/services/signup.ts
@@ -0,0 +1,20 @@
+import { sleep } from "../utils/sleep";
+import { httpClient } from "./httpClient";
+
+export interface SignupParams {
+  name: string;
+  email: string;
+  password: string;
+}
+
+interface SignupReponse {
+  accessToken: string
+}
+
+export async function signup(params: SignupParams) {
+  await sleep();
+
+  const { data } = await httpClient.post<SignupReponse>('/auth/company/signup', params);
+
+  return data;
+}
