32c43d20328ab9433c967c6dd924f5fdc67b7d5f
Vinicius Braznick <viniciusbraznick@gmail.com>
2024-01-29 21:45:36 -0300

    feat: adding candidate routes

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -12,8 +12,7 @@ export function Router() {
       <Routes>
         <Route element={<Auth isPrivate={false} />}>
           <Route element={<AuthLayout/>}>
-            <Route path='/login' element={<Login />}/>
-            <Route path='/' element={<Login />}/>
+            <Route path='*' element={<Login />}/>
             <Route path='/candidate/register' element={<Register />}/>
             <Route path='/company/register' element={<Register />}/>
           </Route>
@@ -22,17 +21,24 @@ export function Router() {
         <Route element={<DashboardLayout/>}>
           <Route element={<Auth isPrivate={true} userAllowed="candidate" />}>
             <Route path='/candidate/dashboard' element={<Dashboard />}/>
+            <Route path='/chat' element={<Dashboard />}/>
+            <Route path='/candidate/curriculum' element={<Dashboard />}/>
+            <Route path='/candidate/security' element={<Dashboard />}/>
+            <Route path='/candidate/data' element={<Dashboard />}/>
+            <Route path='/candidate/privacy' element={<Dashboard />}/>
+            <Route path='/candidate/applications' element={<Dashboard />}/>
+            <Route path='/candidate/preferences' element={<Dashboard />}/>
           </Route>
 
           <Route element={<Auth isPrivate={true} userAllowed="company" />}>
-          <Route path='/company/dashboard' element={<Dashboard />}/>
-              <Route path='/chat' element={<Dashboard />}/>
-              <Route path='/company/curriculum' element={<Dashboard />}/>
-              <Route path='/company/security' element={<Dashboard />}/>
-              <Route path='/company/data' element={<Dashboard />}/>
-              <Route path='/company/privacy' element={<Dashboard />}/>
-              <Route path='/company/applications' element={<Dashboard />}/>
-              <Route path='/company/preferences' element={<Dashboard />}/>
+            <Route path='/company/dashboard' element={<Dashboard />}/>
+            <Route path='/chat' element={<Dashboard />}/>
+            <Route path='/company/curriculum' element={<Dashboard />}/>
+            <Route path='/company/security' element={<Dashboard />}/>
+            <Route path='/company/data' element={<Dashboard />}/>
+            <Route path='/company/privacy' element={<Dashboard />}/>
+            <Route path='/company/applications' element={<Dashboard />}/>
+            <Route path='/company/preferences' element={<Dashboard />}/>
           </Route>
         </Route>
       </Routes>
