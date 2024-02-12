f56a55cb05942bc4c016452ce479424608a53c22
Vinicius Braznick <viniciusbraznick@gmail.com>
2024-02-12 17:03:00 -0300

    feat: create unique route to edit account data

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -6,9 +6,7 @@ import { Register } from '../view/pages/Register';
 import { Dashboard } from '../view/pages/Dashboard';
 import { AuthLayout } from '../view/layouts/Auth';
 import { DashboardLayout } from '../view/layouts/Dashboard';
-import AccountData from '../view/pages/AccountData';
 import ConfigAccountData from '../view/pages/ConfigAccount';
-
 
 export function Router() {
   return(
@@ -25,14 +23,13 @@ export function Router() {
         <Route element={<DashboardLayout/>}>
           <Route element={<Auth isPrivate={true} />}>
             <Route path='/chat' element={<Dashboard />}/>
+            <Route path='/data' element={<ConfigAccountData />}/>
           </Route>
 
           <Route element={<Auth isPrivate={true} userAllowed="candidate" />}>
             <Route path='/candidate/dashboard' element={<Dashboard />}/>
             <Route path='/candidate/curriculum' element={<Dashboard />}/>
             <Route path='/candidate/security' element={<Dashboard />}/>
-            <Route path='/candidate/data' element={<AccountData />}/>
-            <Route path='/data' element={<ConfigAccountData />}/>
             <Route path='/candidate/privacy' element={<Dashboard />}/>
             <Route path='/candidate/applications' element={<Dashboard />}/>
             <Route path='/candidate/preferences' element={<Dashboard />}/>
@@ -42,7 +39,6 @@ export function Router() {
             <Route path='/company/dashboard' element={<Dashboard />}/>
             <Route path='/company/curriculum' element={<Dashboard />}/>
             <Route path='/company/security' element={<Dashboard />}/>
-            <Route path='/company/data' element={<AccountData />}/>
             <Route path='/company/privacy' element={<Dashboard />}/>
             <Route path='/company/applications' element={<Dashboard />}/>
             <Route path='/company/preferences' element={<Dashboard />}/>
